31a35fbecffc147299d2b82cfb1039cc23313303
Anen0 <Lum3nGee@gmail.com>
2026-04-10 13:52:09 +0800

    removed unused endpoints

--- flaskapp/py_routes/main_routes/general_routes.py
+++ flaskapp/py_routes/main_routes/general_routes.py
@@ -47,48 +47,35 @@ def main_dashboard():
 
 
 
-@main.route('/crud_view', methods=['GET'])
-def crud_view():
-    if request.method == 'GET':
-        views = Crud_tbl.query.all()
-        tmp_list = list()
-        for i in views:
-            k = {
-                'ct_id'        : i.ct_id,
-                'some_string'  : i.some_string,
-                'some_text'    : i.some_text,
-                'some_int'     : i.some_int,
-                'dtg'     : i.dtg
-            }
-            tmp_list.append(k)
-        # return jsonify(tmp)
+# @main.route('/crud_view', methods=['GET'])
+# def crud_view():
+#     if request.method == 'GET':
+#         views = Crud_tbl.query.all()
+#         tmp_list = list()
+#         for i in views:
+#             k = {
+#                 'ct_id'        : i.ct_id,
+#                 'some_string'  : i.some_string,
+#                 'some_text'    : i.some_text,
+#                 'some_int'     : i.some_int,
+#                 'dtg'     : i.dtg
+#             }
+#             tmp_list.append(k)
+#         # return jsonify(tmp)
 
-            tmp = table_builder.crud_dt_serverside(request, tmp_list)
-        return jsonify(tmp)
+#             tmp = table_builder.crud_dt_serverside(request, tmp_list)
+#         return jsonify(tmp)
         
 
 
-@main.route('/crud_update', methods=['GET', 'POST'])
-def crud_update():
-    pass
+# @main.route('/crud_update', methods=['GET', 'POST'])
+# def crud_update():
+#     pass
 
-@main.route('/crud_delete', methods=['GET', 'POST'])
-def crud_delete():
-    pass
+# @main.route('/crud_delete', methods=['GET', 'POST'])
+# def crud_delete():
+#     pass
 
-
-
-@main.route('/dashboard_h', methods=['GET', 'POST'])
-def main_dashboard_h():
-    page_title = 'Main page - Horizontal'
-
-    if request.method == 'GET':
-        return render_template('general/dashboard_H.html', page_title=page_title)
-
-
-
-# CRUD page
-# ===================================================================
 
 
 
