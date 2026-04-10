fddd8d0f53c39fbdf33948c3a0592c9fc707ccaa
Anen0 <Lum3nGee@gmail.com>
2026-04-10 16:48:02 +0800

    navbar adjustments

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -8,8 +8,7 @@ class Config:
 
 
     # SQLALCHEMY CONFIGS
-    # SQLALCHEMY_DATABASE_URI = "mysql://nomad:{}@localhost/flaskapp?charset=utf8mb4".format(url_quote('qwe123!@#'))
-    SQLALCHEMY_DATABASE_URI = "mysql://nomad:{}@localhost/flaskapp".format(url_quote('qwe123!@#'))
+    SQLALCHEMY_DATABASE_URI = "mysql://nomad:{}@localhost/flaskapp?charset=utf8mb4".format(url_quote('qwe123!@#'))
     # SQLALCHEMY_BINDS = {
     #     'vetting': "mysql://nomad:{}@localhost/vetting?charset=utf8mb4".format(url_quote('qwe123!@#')),
     #     'db_test': 
